542df37efc5e7b6a3f36a2b571e23fac2675b51d
Catfallen <markimprogramador2006@gmail.com>
2025-07-26 23:45:44 -0300

    api url

--- public/scripts/ip.js
+++ public/scripts/ip.js
@@ -1,3 +1,3 @@
 //var api_url = 'http://192.168.18.230:3000'
-var api_url = 'http://192.168.18.230:3000'
-//var api_url = 'https://lista-de-tarefas-gt3z.onrender.com'
+//var api_url = 'http://192.168.18.230:3000'
+var api_url = 'https://lista-de-tarefas-gt3z.onrender.com'
